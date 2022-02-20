ce5c02f1b0dafe78be59943e918605cc7b740e22
Rodolfo Mariano <rodolfo.mariano@outlook.com>
2022-02-20 08:11:00 -0300

    feat: created Splashs

--- src/screens/SplashOne/index.tsx
+++ src/screens/SplashOne/index.tsx
@@ -1,10 +1,113 @@
+import { useState } from 'react'
+import { useNavigation } from '@react-navigation/native'
+
+import { Ionicons } from '@expo/vector-icons'
+
 import {
-  Container
+  Container,
+  GoToBackButton,
+  Content,
+  ImageBanner,
+  Title,
+  Description,
+  Footer,
+  ProgressContainer,
+  Circle,
+  NextButton,
+  NextButtonTitle,
+  JumpButton,
+  JumpButtonTitle,
 } from './styles'
 
 export function SplashOne() {
+  const [progress, setProgress] = useState(0)
+
+  const navigation = useNavigation()
+
+  function handleGoToBack() {
+    progress === 0 && navigation.goBack()
+    progress > 0 && setProgress(progress - 1)
+  }
+
+  function handleNext() {
+    setProgress(progress + 1)
+  }
+
   return (
     <Container>
+
+      <GoToBackButton
+        onPress={handleGoToBack}
+      >
+        <Ionicons name="arrow-back-sharp" size={32} color="#FBC105" />
+      </GoToBackButton>
+
+      <Content>
+        {progress === 0 && (
+          <ImageBanner
+            source={{
+              uri: 'https://firebasestorage.googleapis.com/v0/b/imagens-3dce0.appspot.com/o/image_splash_one.png?alt=media&token=e10efbb7-f74a-45b3-bdc0-8ccf2c6ab6f5'
+            }}
+          />
+        )}
+
+        {progress === 1 && (
+          <ImageBanner
+            source={{
+              uri: 'https://firebasestorage.googleapis.com/v0/b/imagens-3dce0.appspot.com/o/image_splash_two.png?alt=media&token=886ffdfb-3005-4f1c-ab72-5dab501a609d'
+            }}
+          />
+        )}
+
+        {progress === 2 && (
+          <ImageBanner
+            source={{
+              uri: 'https://firebasestorage.googleapis.com/v0/b/imagens-3dce0.appspot.com/o/image_splash_tree.png?alt=media&token=99093542-605d-46fb-8624-729082d22f61'
+            }}
+          />
+        )}
+
+        <Title>
+          {progress === 0
+            ? 'Como os sentimentos influenciam nossas escolhas financeiras?'
+            : progress === 1
+              ? 'Por que o Open Finance na XP é diferente?'
+              : 'Fora o controle de gastos e recebimentos, como você se sente?'
+          }
+
+        </Title>
+
+        <Description>
+          {progress === 0
+            ? 'Com seus dados compartilhados, a XP te mostrará o que suas movimentações financeiras dizem sobre você e como sua autopercepção impacta suas finanças.'
+            : progress === 1
+              ? 'Além de receber as melhores ofertas de produtos e serviços, acesse o termômetro da sua saúde financeira e receba dicas para prosperar financeiramente.'
+              : 'Acompanhe sua evolução com gráficos. Além de fazer a gestão pessoal de finanças, identifique quando está otimista e sem preocupação em relação à sua vida financeira.'
+          }
+
+        </Description>
+
+        <ProgressContainer>
+          <Circle style={{ backgroundColor: `${progress === 0 ? '#DADADA' : '#3D3D3D'}`, marginRight: 4 }} />
+          <Circle style={{ backgroundColor: `${progress === 1 ? '#DADADA' : '#3D3D3D'}`, marginRight: 4 }} />
+          <Circle style={{ backgroundColor: `${progress === 2 ? '#DADADA' : '#3D3D3D'}` }} />
+        </ProgressContainer>
+
+      </Content>
+
+      <Footer>
+        <NextButton
+          onPress={handleNext}
+        >
+          <NextButtonTitle>CONTINUAR</NextButtonTitle>
+        </NextButton>
+
+        <JumpButton>
+          <JumpButtonTitle>
+            Pular essa etapa
+          </JumpButtonTitle>
+        </JumpButton>
+      </Footer>
 
     </Container>
   )
--- src/screens/SplashOne/styles.ts
+++ src/screens/SplashOne/styles.ts
@@ -1,9 +1,99 @@
 import styled from "styled-components/native";
 
 export const Container = styled.View`
+  position: relative;
   flex: 1;
   margin-top: 24px;
   padding-left: 8px;
   padding-right: 8px;
   background-color: ${({ theme }) => theme.colors.background};
+  padding-top: 128px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const GoToBackButton = styled.TouchableOpacity`
+ position: absolute;
+ top: 24px;
+ left: 24px;
+`
+
+export const Content = styled.View`
+  padding-left: 32px;
+  padding-right: 32px;
+
+  display: flex;
+  align-items: center;
+`
+
+export const ImageBanner = styled.Image`
+  width: 200px;
+  height: 220px;
+  margin-bottom: 48px;
+`
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.colors.title};
+  font-size: 16px;
+  font-family: ${({ theme }) => theme.fonts.poppins_regular};
+  text-align: center;
+  margin-bottom: 24px;
+`
+
+export const Description = styled.Text`
+  color: ${({ theme }) => theme.colors.text_card};
+  font-size: 14px;
+  font-family: ${({ theme }) => theme.fonts.poppins_regular};
+  text-align: center;
+`
+
+export const ProgressContainer = styled.View`
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+`
+
+export const Circle = styled.View`
+  width: 8px;
+  height: 8px;
+  margin-top: 8px;
+  border-radius: 4px;
+  background-color: ${({ theme }) => theme.colors.shape_200};
+`
+
+export const Footer = styled.View`
+  width: 100%;
+  padding: 0 24px;
+
+  display: flex;
+  align-items: center;
+`
+
+export const NextButton = styled.TouchableOpacity`
+  width: 100%;
+  padding: 10px 0;
+  background-color: ${({ theme }) => theme.colors.icons};
+  border-radius: 8px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const NextButtonTitle = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.poppins_bold};
+  font-size: 16px;
+`
+
+export const JumpButton = styled.TouchableOpacity`
+  margin-bottom: 72px;
+  margin-top: 32px;
+`
+
+export const JumpButtonTitle = styled.Text`
+  color: ${({ theme }) => theme.colors.title};
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: 16px;
 `
